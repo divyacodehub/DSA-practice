313272824d432ddd00389d863c31ad1a93179880
Divya Mehta <mehtadivya776@gmail.com>
2026-09-13 16:53:58 +0530

    Add more tree problems and implementations

---
+++ Trees/max_depth_of_binary_tree.cpp
@@ -0,0 +1,31 @@
+// LeetCode 104 - Maximum Depth of Binary Tree
+
+// Approach: Recursion (DFS)
+
+// Time: O(n)
+
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+        return 0;
+
+        int left = maxDepth(root -> left);
+        int right = maxDepth(root -> right);
+
+        return 1 + max(left,right);
+    }
+};
---
+++ Trees/zigzag_level_order_traversal.cpp
@@ -0,0 +1,62 @@
+// LeetCode 103 - Binary Tree Zigzag Level Order Traversal
+
+// Approach: BFS using Queue + Reverse
+
+// Time: O(n)
+
+// Space: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if (root == NULL)
+        return ans;
+
+        queue<TreeNode*> q;
+
+        q.push(root);
+
+        bool leftToRight = true;
+
+        while(!q.empty()){
+            int size = q.size();
+
+            vector<int> level;
+
+            for(int i = 0; i < size; i++){
+
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node -> val);
+
+                if(node -> left != NULL)
+                q.push(node -> left);
+               
+                if(node -> right != NULL)
+                q.push(node -> right);
+            }
+            
+            if(!leftToRight)
+            reverse(level.begin(),level.end());
+
+            ans.push_back(level);
+
+            leftToRight = !leftToRight;
+        }
+        return ans;
+    }
+};
