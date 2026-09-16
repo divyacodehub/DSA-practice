406a5105b536d31e44c62d402cd1e794e3070882
Divya Mehta <mehtadivya776@gmail.com>
2026-09-17 00:27:56 +0530

    path sum questions added

---
+++ Trees/path_sum.cpp
@@ -0,0 +1,35 @@
+// LeetCode 112 - Path Sum
+
+// Approach: Recursion (DFS)
+
+// Time: O(n)
+
+// Space: O(h)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL)
+        return false;
+
+        if(root -> left == NULL && root -> right == NULL)
+        return targetSum == root -> val;
+
+        int remaining = targetSum - root -> val;
+
+        return hasPathSum(root -> left, remaining) ||
+               hasPathSum(root -> right, remaining);
+    }
+};
---
+++ Trees/path_sum_II.cpp
@@ -0,0 +1,52 @@
+// LeetCode 113 - Path Sum II
+
+// Approach: Recursion + Backtracking
+
+// Time: O(n)
+
+// Space: O(h)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+    void solve(TreeNode* root, int targetSum){
+        if(root == NULL)
+        return;
+
+        path.push_back(root -> val);
+
+        targetSum = targetSum - root -> val;
+
+        if(root -> left == NULL && root -> right == NULL){
+
+        if(targetSum == 0)
+        ans.push_back(path);
+
+        path.pop_back();
+        return;
+        }
+
+        solve(root -> left, targetSum);
+        solve(root -> right, targetSum);
+
+        path.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+    solve(root, targetSum);
+    return ans;
+    }
+};
