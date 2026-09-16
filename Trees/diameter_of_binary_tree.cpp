a34198f36915aeca900d09f87afee922686caf67
Divya Mehta <mehtadivya776@gmail.com>
2026-09-16 13:00:16 +0530

    add more tree practice questions

---
+++ Trees/diameter_of_binary_tree.cpp
@@ -0,0 +1,40 @@
+// LeetCode 543 - Diameter of Binary Tree
+
+// Approach: Recursion (DFS) 
+
+// Time: O(n) 
+
+// Space: O(h)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameter = 0;   
+    int height(TreeNode* root){
+        if(root == NULL)
+        return 0;
+
+        int left = height(root -> left);
+        int right = height (root -> right);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return diameter;
+    }
+};
---
+++ Trees/symmetric_tree.cpp
@@ -0,0 +1,44 @@
+// LeetCode 101 - Symmetric Tree
+
+// Approach: Recursion (DFS)
+
+// Time: O(n)
+
+// Space: O(h)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+        return true;
+
+        return isMirror(root -> left, root -> right);
+    }
+
+    bool isMirror(TreeNode* p, TreeNode* q){
+        if(p == NULL && q == NULL)
+        return true;
+
+        if(p == NULL || q == NULL)
+        return false;
+
+        if(p -> val != q -> val)
+        return false;
+
+        return isMirror(p -> left, q -> right) &&
+               isMirror(q -> left, p -> right);
+
+    }
+};
